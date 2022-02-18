7f0767e3a7251aa17d3706c5f3527120d666b6e9
levi <glendenning.levi@gmail.com>
2022-02-17 19:29:30 -0700

    added to the modules folder

---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,19 @@
+function autocomplete(input, latInput, lngInput) {
+if(!input) return; // skip this function from running if there is not input on the page
+
+const dropdown= new google.maps.places.Autocomplete(input);
+
+dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+    latInput.value = places.geometry.location.lat();
+    lngInput.value = places.geometry.location.lng();
+});
+// if someone hits enter on the address field, don't submit the form
+
+input.on('keydown', (e) => {
+    if(e.keycode === 13 ) e.preventDefult();
+});
+
+}
+
+export default autocomplete;
